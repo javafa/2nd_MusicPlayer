5386b701c6b1f87da0ec1db61da51aa5966edcda
javafa <nigne2016@gmail.com>
2017-02-02 18:00:04 +0900

    seekbar 연동

--- app/src/main/java/com/veryworks/android/musicplayer/PlayerActivity.java
+++ app/src/main/java/com/veryworks/android/musicplayer/PlayerActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.ContactsContract;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -24,6 +27,7 @@ public class PlayerActivity extends AppCompatActivity {
 
     MediaPlayer player;
     SeekBar seekBar;
+    TextView txtDuration;
 
     // 플레이어 상태 플래그
     private static final int PLAY = 0;
@@ -35,15 +39,35 @@ public class PlayerActivity extends AppCompatActivity {
 
     int position = 0; // 현재 음악 위치
 
+    // 핸들러 상태 플래그
+    public static final int PROGRESS_SET = 101;
+
+    // 핸들러
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
+            switch(msg.what){
+                case PROGRESS_SET:
+                    if(player != null)
+                        seekBar.setProgress(player.getCurrentPosition());
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        playStatus = STOP;
+
         // 볼륨 조절 버튼으로 미디어 음량만 조절하기 위한 설정
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+        txtDuration = (TextView) findViewById(R.id.txtDuration);
 
         btnRew = (ImageButton) findViewById(R.id.btnRew);
         btnPlay = (ImageButton) findViewById(R.id.btnPlay);
@@ -100,10 +124,30 @@ public class PlayerActivity extends AppCompatActivity {
                 // 플레이어에 음원 세팅
                 player = MediaPlayer.create(this, musicUri);
                 player.setLooping(false); // 반복여부
+
+                // seekBar 길이
+                seekBar.setMax(player.getDuration());
+                txtDuration.setText(player.getDuration()/1000 + " Sec.");
                 player.start();
 
                 playStatus = PLAY;
                 btnPlay.setImageResource(android.R.drawable.ic_media_pause);
+
+                // sub thread 를 생성해서 mediaplayer 의 현재 포지션 값으로 seekbar 를 변경해준다. 매 1초마다
+                new Thread() {
+                    @Override
+                    public void run() {
+                        while (playStatus < STOP) {
+                            Logger.print("position=========" + player.getCurrentPosition(), "PlayerActivity");
+                            handler.sendEmptyMessage(PROGRESS_SET);
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                            }
+                        }
+                    }
+                }.start();
+
                 break;
             // 플레이중이면 멈춤
             case PLAY :
@@ -128,5 +172,13 @@ public class PlayerActivity extends AppCompatActivity {
 
     private void next() {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(player != null){
+            player.release(); // 사용이 끝나면 해제해야만 한다.
+        }
     }
 }
